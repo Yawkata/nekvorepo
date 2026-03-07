ce942ba1160523a4dc9e4e2617f66404d795c6fa
Filip05-TUES <f.ermenkov@gmail.com>
2026-03-07 18:54:42 +0200

    Create DEV IAM user and add new outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,21 @@ output "cognito_pool_id" {
 output "cognito_client_id" {
   value = aws_cognito_user_pool_client.client.id
 }
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.repo_storage.id
+}
+
+output "dev_access_key_id" {
+  value = aws_iam_access_key.dev_key.id
+}
+
+output "dev_secret_access_key" {
+  value     = aws_iam_access_key.dev_key.secret
+  sensitive = true
+}
+
+output "cognito_client_secret" {
+  value     = aws_cognito_user_pool_client.client.client_secret
+  sensitive = true
+}
